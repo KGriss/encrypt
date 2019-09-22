a702e9d1c1b3eb3c011309346f33244b733defa4
Griss <37448271+KGriss@users.noreply.github.com>
2019-09-22 22:05:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 document.getElementById("crypt").addEventListener("input", encrypt);
-let width = getComputedStyle(document.getElementById("crypt"));
-console.log(width.height);
 
 function encrypt(e) {
 	let msg = e.target.value;
-	console.log(msg);
 	let chars = "²&é\"'(-è_çà)=azertyuiop^$qsdfghjklmù*wxcvbn,;:!1234567890°+AZERTYUIOP¨£QSDFGHJKLM%µWXCVBN?./§<>~#{[|`@]}€¤ \\êï";
-	let code = ""
+	let code = "";
 	
 	for (let i=0;i<msg.length;i++) {
+		let inChars = false;
 		for (let key=0;key<chars.length;key++) {
 			if (msg[i] === chars[key]) {
+				inChars = true;
 				code += chars[(key+chars.length/2)%chars.length];
-				document.getElementById("crypted").textContent = code;
 			}
 		}
+		if (! inChars) code += msg[i];
 	}
+	document.getElementById("crypted").textContent = code;
 }
